27b436d7e856f8e66a9fe11553f72157123e5ddc
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-10 13:33:20 -0500

    Fix Dropdown List and alerts

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,10 +2,16 @@ class ExpensesController < ApplicationController
 
   def index
     @tab = :expenses
-    @expenses = Expense.all.order(date: :desc)
-    @types = load_filter_expenses( @expenses, "type" )
-    @categories = load_filter_expenses( @expenses, "category")
+    @expenses = Expense.where("date >= :start_date AND date <= :end_date", {
+                              start_date: Date.current.beginning_of_month,
+                              end_date: Date.current.end_of_month
+                              }).order(date: :desc)
+    @types = load_filter_expenses( Expense.all, "type" )
+    @categories = load_filter_expenses( Expense.all, "category")
   end
+
+  puts ( Date.today - 365 ).beginning_of_month
+  puts ( Date.today ).beginning_of_month
 
   def new
     respond_to do |format|
@@ -14,6 +20,7 @@ class ExpensesController < ApplicationController
     end
     @expense = Expense.new
     @categories = load_filter_expenses( Expense.all, "category")
+    @types = load_filter_expenses( Expense.all, "type")
   end
 
   def create
@@ -23,10 +30,12 @@ class ExpensesController < ApplicationController
   def show
     @expense = Expense.find(params[:id])
     @categories = load_filter_expenses( Expense.all, "category")
+    @types = load_filter_expenses( Expense.all, "type")
   end
 
   def update
     @expense = Expense.find(params[:id])
+    @prev_expense = Expense.find(params[:id])
     @expense.update(params_expense)
   end
 
@@ -44,10 +53,18 @@ class ExpensesController < ApplicationController
         end
       end
       @expenses = Expense.where("type == :filter", { filter: @type_index }).order(date: :desc)
+    # Here I go, validate month and filter by its, after that make the logic of view for this filter
+    elsif Date::MONTHNAMES.include?(@filter)
+      @month = @filter
+      @expenses = Expense.where("date >= :start_date AND date <= :end_date",
+                                { 
+                                  start_date: Date.parse(@filter).beginning_of_month,
+                                  end_date: Date.parse(@filter).end_of_month
+                                }
+                              ).order(date: :desc)
     else
       @expenses = Expense.where("category == :filter", { filter: @filter }).order(date: :desc)
     end
-
   end
 
   private
