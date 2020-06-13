53d6e3b1a64a9fbabfafec0b4bd2d51daab914cc
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-12 21:56:54 -0500

    Delete migrations useless

--- db/migrate/20200605203037_remove_type_from_expenses.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTypeFromExpenses < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :expenses, :type, :string
-  end
-end
--- db/migrate/20200605203546_add_exp_type_to_expenses.rb
+++
@@ -1,5 +0,0 @@
-class AddExpTypeToExpenses < ActiveRecord::Migration[6.0]
-  def change
-    add_column :expenses, :exp_type, :string
-  end
-end
