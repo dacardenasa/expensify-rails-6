836ea0f4689a25a16ee74d263df865a126829926
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-12 23:02:36 -0500

    Update migration rename_column

--- db/migrate/20200613033836_rename_exp_type_name_to_type_name.rb
+++ db/migrate/20200613033836_rename_exp_type_name_to_type_name.rb
@@ -1,7 +1,5 @@
 class RenameExpTypeNameToTypeName < ActiveRecord::Migration[6.0]
   def change
-    def change
-      rename_column :expenses, :exp_type, :type
-    end
+    rename_column :expenses, :exp_type, :type
   end
 end
