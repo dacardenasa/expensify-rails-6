c0efe10d7072a8067e86e521f664cedb19ddb6d2
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-19 11:42:01 -0500

    Update travis chrome version

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,14 @@ require 'capybara/minitest'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 
+# Capybara.register_driver :chrome do |app|
+# 	options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+# 
+# 	Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+# end
+# 
+# Capybara.javascript_driver = :chrome
+
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
